33815d293b6436f06388a6a8db35f5cb47e97010
Ceren ÖZTÜRK <190118019@ogr.gelisim.edu.tr>
2023-02-23 19:08:02 +0300

    düzenleme

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,28 +1,154 @@
-import React, {useRef} from "react";
+import React, { useRef } from "react";
 //ICONS
-import { AiOutlineMail, AiFillGithub, AiOutlineInstagram, AiFillLinkedin } from "react-icons/ai";
-import {MdLocationOn} from 'react-icons/md'
-import emailjs from '@emailjs/browser';
+import {
+  AiOutlineMail,
+  AiFillGithub,
+  AiOutlineInstagram,
+  AiFillLinkedin,
+} from "react-icons/ai";
+import { MdLocationOn } from "react-icons/md";
+import { BsTelephone } from "react-icons/bs";
 
-const Contact  = () => {
+import emailjs from "@emailjs/browser";
+
+const Contact = () => {
   const form = useRef();
-  
+
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_vzsy72x', 'template_d4jms4t', form.current, 'AKbBq1ji8pd6eaOsl')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_vzsy72x",
+        "template_d4jms4t",
+        form.current,
+        "AKbBq1ji8pd6eaOsl"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
           console.log("message sent");
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
   };
   return (
     <div className="contact-container" id="contact">
-      <h1 >CONTACT </h1>
-     
-      <div className="contact-div">
+      <div>
+        <h1 className="section-header">CONTACT</h1>
+        <div className="contact-wrapper">
+          <form ref={form}
+            id="contact-form"
+            className="form-horizontal"
+            role="form"
+            onSubmit={sendEmail}
+          >
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                placeholder="NAME"
+                name="user_name"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="EMAIL"
+                name="user_email"
+                required
+              />
+            </div>
+            <textarea
+              className="form-control"
+              rows={10}
+              placeholder="MESSAGE"
+              name="message"
+              required
+              
+            />
+            <button
+              className="btn btn-primary send-button"
+              id="submit"
+              type="submit"
+              value="SEND"
+            >
+              <div className="alt-send-button">
+                <i className="fa fa-paper-plane" />
+                <span className="send-text">SEND</span>
+              </div>
+            </button>
+          </form>
+
+          <div className="direct-contact-container">
+            <ul className="contact-list">
+              <li className="list-item">
+                <span className="contact-text place">
+                  <MdLocationOn className="contact-icons" />
+                  Istanbul, Turkey
+                </span>
+              </li>
+              <li className="list-item">
+                <a
+                  className="contact-text phone"
+                  href="tel:1-212-555-5555"
+                  title="Give me a call"
+                >
+                  <BsTelephone className="contact-icons" />
+                  (538) 822-7934
+                </a>
+              </li>
+              <li className="list-item">
+                <a
+                  className="contact-text gmail"
+                  href="mailto:#"
+                  title="Send me an email"
+                >
+                  <AiOutlineMail className="contact-icons" />
+                  cerenoztrk69@gmail.com
+                </a>
+              </li>
+            </ul>
+            <hr />
+            <ul className="social-media-list">
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/ceren-%C3%B6zt%C3%BCrk-1514721b1/"
+                  target="_blank"
+                >
+                  <AiFillLinkedin className="social-icons" />
+
+                  {/* <i className="fa fa-github" aria-hidden="true" /> */}
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/cerenoztrk" target="_blank">
+                  <AiFillGithub className="contact-icon" />
+                  {/* <i className="fa fa-twitter" aria-hidden="true" /> */}
+                </a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com/crnnoztrrk/" target="_blank">
+                  <AiOutlineInstagram className="social-icons" />
+                  {/* <i className="fa fa-instagram" aria-hidden="true" /> */}
+                </a>
+              </li>
+            </ul>
+            <hr />
+            <div className="copyright">
+              If you want to see my resume, just send a message.
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="contact-div">
         <div className="contact-info">
         <p className="location"><MdLocationOn size={'20px'}/> Istanbul/Turkey</p>
         <p className="mail">
@@ -34,18 +160,17 @@ const Contact  = () => {
         <a href="https://github.com/cerenoztrk" target="_blank"><AiFillGithub className="social-icons"/>github.com/cerenoztrk</a>
         <a href="https://www.instagram.com/crnnoztrrk/" target="_blank" ><AiOutlineInstagram className="social-icons"/>instagram.com/crnnoztrrk</a>
         </div>
-      </div>
-        
-        <form ref={form} onSubmit={sendEmail}>
+      </div> */}
+
+      {/* <form ref={form} onSubmit={sendEmail}>
         <h3>If you want to see my resume, just send a message. </h3>
         <input required autoComplete="off" type="text" name="user_name" placeholder="Enter your name" />
         <input required autoComplete="off" type="email" name="user_email" placeholder="Enter your email"/>
         <textarea required autoComplete="off" name="message" placeholder="Enter your message" />
         <input type="submit" value="Send" />
-        </form>
-      
+        </form> */}
     </div>
   );
 };
 
-export default Contact ;
+export default Contact;
--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,5 +1,10 @@
 import React from "react";
-
+import { AiFillHtml5, AiOutlineConsoleSql, AiFillGithub } from "react-icons/ai";
+import { DiCss3, DiJavascript1, DiJava } from "react-icons/di";
+import { FaReact } from "react-icons/fa";
+import { TbCSharp } from "react-icons/tb";
+import { FiFigma } from "react-icons/fi";
+import { SiAdobephotoshop } from "react-icons/si";
 const Skills = () => {
   return (
     <div className="ability-container" id="skills">
@@ -8,27 +13,76 @@ const Skills = () => {
         <div className="frontend">
           <ul>
             <h1>FRONT-END</h1>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>ReactJS</li>
+            <li className="list-items">
+              <span className="skills-icon">
+              <AiFillHtml5 className="app-icons" />
+              HTML
+              </span>
+            </li>
+            <li className="list-items">
+              <span className="skills-icon">
+              <DiCss3 className="app-icons" />
+              CSS
+              </span>
+            </li>
+            <li className="list-items">
+              <span className="skills-icon">
+              <DiJavascript1 className="app-icons" />
+              JavaScript
+              </span>
+            </li>
+            <li className="list-items">
+              <span className="skills-icon">
+              <FaReact className="app-icons" />
+              ReactJS
+              </span>
+            </li>
           </ul>
         </div>
         <div className="backend">
           <ul>
             <h1>BACKEND</h1>
-            <li>Java</li>
-            <li>C#</li>
-            <li>SQL</li>
-            <li>ASP.Net</li>
+            <li className="list-items">
+              <span className="skills-icon">
+              <DiJava className="app-icons" />
+              Java
+              </span>
+            </li>
+            <li className="list-items">
+               <span className="skills-icon">
+              <TbCSharp className="app-icons" />
+              C#
+              </span>
+            </li>
+            <li className="list-items">
+              <span className="skills-icon">
+              <AiOutlineConsoleSql className="app-icons" />
+              SQL
+              </span>
+            </li>
           </ul>
         </div>
         <div className="outros">
           <ul>
             <h1>OUTROS</h1>
-            <li>Github</li>
-            <li>Figma</li>
-            <li>Adobe ps</li>
+            <li className="list-items">
+              <span className="skills-icon">
+              <AiFillGithub className="app-icons" />
+              Github
+              </span>
+            </li>
+            <li className="list-items">
+              <span className="skills-icon">
+              <FiFigma className="app-icons" />
+              Figma
+              </span>
+            </li>
+            <li className="list-items">
+              <span className="skills-icon">
+              <SiAdobephotoshop className="app-icons" />
+              Adobe ps
+              </span>
+            </li>
           </ul>
         </div>
       </div>
